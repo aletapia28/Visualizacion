ff07f757752291d23d89412fed56d35cde3cc452
aletapia28 <aletapia28@hotmail.com>
2020-05-12 13:57:36 -0600

    [improved]: separando clases

---
+++ android/lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'HomePage.dart';
+
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: true,
+      title: 'Visualizacion de la informacion ',
+      theme: ThemeData(
+        primaryColor: Colors.red,
+      ),
+      home: HomePage(),
+    );
+  }
+}
